49aed0777b6c800e217d14e7dbb6eb2a8e6ccdeb
MOSOC <warshightide@gmail.com>
2015-11-15 15:11:24 +0000

    Do Basic Settings and Set an Api for GET

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+Genre = require('./models/genre');
+
 //MongoDB Setting
-mongoose.connect('mongodb://localhost/practice')
+mongoose.connect('mongodb://localhost/books')
 var db = mongoose.connection;
 app.get('/',function(req, res){
     res.send('Hola Mongo')
 });
 
+
+app.get('/api/genres', function(req, res){
+	Genre.getGenres(function(err, genres){
+		if(err){
+			throw err;
+		}
+		res.json(genres);
+	});
+});
+
 app.listen(process.env.PORT, process.env.IP);
 console.log('Runnung now...');
+
+
